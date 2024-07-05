b2ec85c3c2663002d26e5fa9817c7930a78c77a9
Chennu05 <120401809+Chennu05@users.noreply.github.com>
2024-07-05 15:22:27 +0530

    Update post_queries.sql

--- queries/post_queries.sql
+++ queries/post_queries.sql
@@ -1,1 +1,15 @@
+SQL queries for managing posts and interactions.
 
+
+
+-- Retrieve all posts
+SELECT * FROM posts;
+
+-- Insert a new post
+INSERT INTO posts (user_id, caption, image_url) VALUES (1, 'New post', 'https://example.com/newimage.jpg');
+
+-- Update post caption
+UPDATE posts SET caption = 'Updated caption' WHERE post_id = 1;
+
+-- Delete a post
+DELETE FROM posts WHERE post_id = 2;
